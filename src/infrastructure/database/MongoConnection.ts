82ef4143974b2e3a57b389079f75f0dc592a348d
luisfelix-93 <lfff6493@gmail.com>
2026-07-07 10:51:39 -0300

    20260707 - conexão com banco de dados e domínio de tenancy

---
+++ src/infrastructure/database/MongoConnection.ts
@@ -0,0 +1,23 @@
+import { Db, MongoClient } from "mongodb";
+
+export class MongoConnection {
+    private static client: MongoClient;
+    private static db: Db;
+
+    static async connect(uri: string, dbName: string): Promise<Db> {
+        if (this.client) {
+            this.client = new MongoClient(uri);
+            await this.client.connect();
+            this.db = this.client.db(dbName);
+            console.log(`[MongoDB] Conectado ao banco de dados: ${dbName}`);
+        }
+        return this.db;
+    }
+
+    static getDb(): Db {
+        if (!this.db) {
+            throw new Error("Sem conexão com o banco de dados");
+        }
+        return this.db;
+    }
+}
